7ae15b6d275c506595761bc0992d1e7db2bb35fa
NaimBiswas <nayeembiswas2@gmail.com>
2022-10-02 23:59:03 +0600

    Add product ready with sweet alert

--- src/app/dashboard/pages/product/add-product/add-product.component.ts
+++ src/app/dashboard/pages/product/add-product/add-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-product',
@@ -7,11 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProductComponent implements OnInit {
   isSubmitSuccess: boolean = false
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
-  saveChanges =()  =>{
+  saveChanges = ()  =>{
     this.isSubmitSuccess = true
+    // this.router.navigateByUrl('/product')
+    Swal.fire('Thank you...', 'Your product has been listed! succesfully!', 'success')
+  }
+  cancelChanges  = async () => {
+    Swal.fire({
+      title: 'Are you sure want to cancelled?',
+      text: 'You will not be able to recover this file!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, cancelled it!',
+      cancelButtonText: 'No, keep it'
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Cancelled!',
+          'Your imaginary file has been cancelled.',
+          'success'
+        )
+        this.router.navigate(['/product'])
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    })
   }
 }
