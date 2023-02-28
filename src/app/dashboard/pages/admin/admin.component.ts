af47ed7c890877fc8ebce21be2c0b27a89632bb4
NaimBiswas <nayeembiswas2@gmail.com>
2023-03-01 00:19:24 +0600

    Dynamic pagination

--- src/app/dashboard/pages/admin/admin.component.ts
+++ src/app/dashboard/pages/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHandler, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { APISEndPoint } from 'src/app/shared/constant/common.constant';
 import { CommonService } from 'src/app/shared/services/common.service';
@@ -12,23 +12,27 @@ import { environment } from 'src/environments/environment';
 export class AdminComponent implements OnInit {
   headers = new HttpHeaders();
   token: string | any
-  users : any
-  response:any
-  constructor(private http:HttpClient, private commonService: CommonService) {
-    this.commonService.getValue('refresh').then((data:any) => {
-      this.token = data
-    })
+  users:any = [];
+  response:any;
+  showDataLoader:boolean = false;
+  constructor(private http:HttpClient, private commonService: CommonService,private next: HttpHandler) {
   }
   
   ngOnInit(): void {
-    this.getAllAdmin('1')
+    this.getAllAdmin(1)
   }
-  getAllAdmin =async (page:any, limit=10,) => {
-    this.http.get(environment.apiUrl+APISEndPoint.getAllAdmin).subscribe((_admins:any) => {
+  getAllAdmin =async (page:any, limit=10) => {  
+    this.showDataLoader = true;
+    this.http.get(environment.apiUrl+APISEndPoint.getAllAdmin,{headers: this.commonService.generatePageChangeHeader(page, limit)}).subscribe((_admins:any) => {
       this.users = _admins.results
       this.response = _admins
+      this.showDataLoader = false
     }, err => {
-
+      console.log(err);
+      // this.commonService.messageToShow(err.error.message, 'middle','error',true,2000)
     })
+  }
+  pageChange = async (page:any) =>{
+    this.getAllAdmin(page)
   }
 }
