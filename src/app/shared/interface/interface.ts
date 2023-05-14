9d58899d2c9b41cca0964e9ff77e1a7d098c04cb
NaimBiswas <nayeembiswas2@gmail.com>
2023-05-15 00:12:24 +0600

    Add: Product Model and Product Interface

--- src/app/shared/interface/interface.ts
+++ src/app/shared/interface/interface.ts
@@ -4,7 +4,24 @@ interface salesRes  {
         orderCount:Number
     } | undefined,
 }
+interface ProductDetails {
+    name: string,
+    manufacturerBrand : string
+    manufacturerName: string
+    price: number
+    discount:number 
+    discountPrice: number
+    category:  CategoryType,
+    description: string,
+    image: string,
+    metaTitle:string,
+    metaKeyword: string,
+    metaDescription:string,
+}
+interface CategoryType { _id: string,  name: string, }
 
 export {
-    salesRes
+    salesRes,
+    ProductDetails,
+    CategoryType
 }
